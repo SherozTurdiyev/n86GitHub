4c3a75e879a2ed739f1d4d3d26834d5a40b03cd6
Sheroz Turdiyev <sherozturdiyev95@gmail.com>
2024-11-16 20:30:01 +0500

    commit

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log("running...");
+
+
+// githubda repo yaratamiz 
+// git repoga nom beriladi
+// git repoga public holatda bulishi kerak ekanligi tanlaysiz
+// git addReadme file qo'shilmaydi
+// git da create qilinadi
+
+// local 
+// git init 
+// git branch -M main
+// git remote add origin 'url'
+// git status 
+// git add . 
+// git commit -m 'commit yoziladi'
+// git push -u origin main   // faqat birinchi commitda shundan 
+// git push    // yuklab berish 
